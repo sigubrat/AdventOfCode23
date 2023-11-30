a9c53e9d51b03e4ce324ccc27f864e56d4b217de
Sigubrat <Sigurdjbratt@hotmail.no>
2023-11-30 08:31:18 +0100

    Prepare repository for day 1

---
+++ Template.scala
@@ -0,0 +1,25 @@
+object DayX {
+  def main(args: Array[String]): Unit = {
+    val input = scala.io.Source.fromFile("input.txt").getLines().toList
+
+    // Part 1
+    val part1 = solvePart1(input)
+    println(s"Part 1: $part1")
+
+    // Part 2
+    val part2 = solvePart2(input)
+    println(s"Part 2: $part2")
+  }
+
+  def solvePart1(input: List[String]): Int = {
+    // Your solution for Part 1 goes here
+    // Example: input.map(_.toInt).sum
+    0
+  }
+
+  def solvePart2(input: List[String]): Int = {
+    // Your solution for Part 2 goes here
+    // Example: input.flatMap(_.split(",")).filter(_.nonEmpty).size
+    0
+  }
+}
---
+++ flake.nix
@@ -0,0 +1,46 @@
+{  
+  description = "aoc23";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (f: p: {
+              sbt = p.sbt.override { jre = p.jdk19_headless; };
+            })
+          ];
+        };
+        jdk = pkgs.jdk19_headless;
+
+        jvmInputs = with pkgs; [
+          jdk
+          coursier
+          sbt
+          scalafmt
+          scala-cli
+        ];
+
+        
+        jvmHook = ''
+          JAVA_HOME="${jdk}"
+        '';
+
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "aoc23";
+          buildInputs = jvmInputs;
+          shellHook = jvmHook;
+        };
+      }
+    );
+
+}
